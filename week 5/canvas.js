38c9e7718a30be6b73de1bb6ce2c08786d34227d
TylerSyn <tcsynott@email.neit.edu>
2022-11-02 13:46:17 -0400

    started java

---
+++ week 5/canvas.js
@@ -0,0 +1,65 @@
+//single line javascript comment
+/*
+multiline javascript comment
+ */
+
+//defines variable to access properties of canvas by ID
+var canvas = document.getElementById("canvas");
+//defines the drawing context of the canvas element
+var ctx = canvas.getContext('2d');
+
+//Draw stuff :)
+//sets up color and outline styles
+ctx.fillStyle = "rgb(0,0,255)";
+ctx.strokeStyle = "green";
+ctx.lineWidth = "5";
+
+//draws a rectangle fillRect(x,y,width,height)
+ctx.fillRect(30,30,200,100);
+ctx.strokeRect(30,30,200,100);
+
+//draw lines
+ctx.beginPath();
+ctx.moveTo(0,0);
+ctx.lineTo(400,250);
+ctx.lineTo(800,0);
+ctx.stroke();
+
+ctx.strokeStyle = "red"
+
+ctx.beginPath();
+ctx.moveTo(800,600);
+ctx.lineTo(400,350);
+ctx.lineTo(0,600);
+ctx.stroke();
+
+//draw circle
+ctx.beginPath();
+ctx.arc(400,300,50,0, (3*Math.PI)/2, false);
+ctx.lineTo(400,300)
+ctx.closePath();
+ctx.fill();
+ctx.stroke()
+
+//some random shape
+ctx.fillStyle = "#55ddef";
+ctx.strokeStyle = "yellow";
+ctx.lineWidth = "2";
+
+ctx.beginPath();
+ctx.moveTo(650,100);
+ctx.lineTo(700,140);
+ctx.lineTo(675,200);
+ctx.lineTo(625,200);
+ctx.lineTo(600,140);
+ctx.closePath();
+ctx.fill();
+ctx.stroke();
+
+//draw  an image to the canvas
+var mario = new Image();
+mario.src = "images/mario.png"
+
+mario.onload = function(){
+    ctx.drawImage(mario, 470, 200, 80, 80);
+}
